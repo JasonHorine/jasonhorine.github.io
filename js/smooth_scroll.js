bea1470bc250a147b74840a46a9eb534b7f3e3dc
Jason Horine <outbackdog@gmail.com>
2016-07-20 13:57:50 -0500

    Adding Bootstrap and overhauling the look to be more normal.

---
+++ js/smooth_scroll.js
@@ -0,0 +1,57 @@
+// scrolling is hidden until other on-page links can be added
+
+
+// from bootstrap docs
+// Add scrollspy to <body>
+$(document).ready(function(){
+  $('body').scrollspy({target: ".navbar", offset: 100});
+
+  // Add smooth scrolling to all links inside a navbar
+  $("#myNavbar a").on('click', function(event){
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash (#)
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }) // End if statement
+    };
+  });
+
+  // Add smooth scrolling to all links inside a navbar
+  // not dry... needs work
+  $(".on_page_link").on('click', function(event){
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash (#)
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      }) // End if statement
+    };
+  });
+
+
+})
