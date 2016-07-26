132b50694b4dc0aa3dc3410a263eedf9bbd219b9
Jason Horine <outbackdog@gmail.com>
2016-07-26 11:40:34 -0500

    removed unused JS, added vet <a>

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -28,30 +28,4 @@ $(document).ready(function(){
       }) // End if statement
     };
   });
-
-  // Add smooth scrolling to all links inside a navbar
-  // not dry... needs work
-  $(".on_page_link").on('click', function(event){
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash (#)
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 700, function(){
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      }) // End if statement
-    };
-  });
-
-
 })
